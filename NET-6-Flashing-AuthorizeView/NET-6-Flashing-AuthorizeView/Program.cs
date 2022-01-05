bbc339886cf01b4b6e1a4b573e7a40ea55d7bde3
Matthew Champion <mattjameschampion@gmail.com>
2022-01-05 21:01:59 +0000

    Add test auth requirement/handler

--- NET-6-Flashing-AuthorizeView/NET-6-Flashing-AuthorizeView/Program.cs
+++ NET-6-Flashing-AuthorizeView/NET-6-Flashing-AuthorizeView/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NET_6_Flashing_AuthorizeView.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy("TestPolicy", policy =>
+    {
+        policy.Requirements.Add(new Requirements.TestRequirement());
+    });
+});
+
+builder.Services.AddSingleton<IAuthorizationHandler, Handlers.TestHandler>();
 
 var app = builder.Build();
 
---
+++ NET-6-Flashing-AuthorizeView/NET-6-Flashing-AuthorizeView/Shared/Auth.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace NET_6_Flashing_AuthorizeView.Shared
+{
+    public static class Requirements
+    {
+        public class TestRequirement : IAuthorizationRequirement
+        {
+        }
+    }
+
+    public static class Handlers
+    {
+        public class TestHandler : AuthorizationHandler<Requirements.TestRequirement>
+        {
+            protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, Requirements.TestRequirement requirement)
+            {
+                await Task.Delay(1);
+                context.Succeed(requirement);
+            }
+        }
+    }
+}
