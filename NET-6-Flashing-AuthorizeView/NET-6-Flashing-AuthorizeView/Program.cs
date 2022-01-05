a03a21b06760ab7e1a069809d8038eb28123d3e9
Matthew Champion <mattjameschampion@gmail.com>
2022-01-05 21:49:59 +0000

    Recreate the project as .NET 5 and migrate across the rendering logic

--- NET-6-Flashing-AuthorizeView/NET-6-Flashing-AuthorizeView/Program.cs
+++ NET-6-Flashing-AuthorizeView/NET-6-Flashing-AuthorizeView/Program.cs
@@ -1,47 +1,20 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using NET_6_Flashing_AuthorizeView.Shared;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddAuthorization(config =>
+namespace NET_6_Flashing_AuthorizeView
 {
-    config.AddPolicy("TaskDelayPolicy", policy =>
+    public class Program
     {
-        policy.Requirements.Add(new Requirements.TaskDelayRequirement());
-    });
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    config.AddPolicy("InstantPolicy", policy =>
-    {
-        policy.Requirements.Add(new Requirements.InstantRequirement());
-    });
-});
-
-builder.Services.AddSingleton<IAuthorizationHandler, Handlers.TaskDelayHandler>();
-builder.Services.AddSingleton<IAuthorizationHandler, Handlers.InstantHandler>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
